9fb20193fd8e9ee566f20e6934dfc9c6657b7099
sago35 <sago35@gmail.com>
2018-11-22 21:44:58 +0900

    Update

--- limichan.go
+++ limichan.go
@@ -1,35 +1,103 @@
 package limichan
 
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
 // Worker ...
 type Worker interface {
+	Do(context.Context, Job) error
+}
+
+// Job ...
+type Job interface {
 }
 
 // Limichan ...
 type Limichan struct {
 	worker chan Worker
+	ctx    context.Context
+	wg     sync.WaitGroup
+	errors []error
+}
+
+// Option ...
+type Option func(*options)
+
+type options struct {
+	maxWorker int
+}
+
+var defaultLimichanOption = options{
+	maxWorker: 1000,
+}
+
+// MaxWorker ...
+func MaxWorker(m int) Option {
+	return func(o *options) {
+		o.maxWorker = m
+	}
 }
 
 // New ...
-func New(maxWorker int) *Limichan {
+func New(ctx context.Context, opt ...Option) *Limichan {
+	opts := defaultLimichanOption
+	for _, o := range opt {
+		o(&opts)
+	}
+
 	lc := &Limichan{
-		worker: make(chan Worker, maxWorker),
+		worker: make(chan Worker, opts.maxWorker),
+		ctx:    ctx,
 	}
 
 	return lc
 }
 
 // AddWorker ...
-func (lc *Limichan) AddWorker(w Worker) {
-	lc.worker <- w
+func (lc *Limichan) AddWorker(w Worker) error {
+	select {
+	case lc.worker <- w:
+		return nil
+	default:
+	}
+
+	return fmt.Errorf("AddWorker() failed")
 }
 
 // Do ...
-func (lc *Limichan) Do(fn func(Worker)) {
+func (lc *Limichan) Do(j Job) error {
 	select {
+	case <-lc.ctx.Done():
+		lc.errors = append(lc.errors, lc.ctx.Err())
+		return lc.ctx.Err()
 	case w := <-lc.worker:
+		lc.wg.Add(1)
 		go func() {
-			fn(w)
+			defer lc.wg.Done()
+			err := w.Do(lc.ctx, j)
+			if err != nil {
+				lc.errors = append(lc.errors, err)
+			}
 			lc.worker <- w
 		}()
 	}
+	return nil
+}
+
+// Wait ...
+func (lc *Limichan) Wait() {
+	lc.wg.Wait()
+}
+
+// Errors ...
+func (lc *Limichan) Errors() []error {
+	return lc.errors
+}
+
+// ClearErrors ...
+func (lc *Limichan) ClearErrors() {
+	lc.errors = lc.errors[:0]
 }
--- limichan_test.go
+++ limichan_test.go
@@ -1,8 +1,9 @@
 package limichan
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"sync"
 	"testing"
 	"time"
 )
@@ -12,35 +13,69 @@ type worker struct {
 	waitMs int
 }
 
-func TestLimichan(t *testing.T) {
-	l := New(5)
+type myJob struct {
+	index int
+}
 
-	w1 := worker{name: "w1", waitMs: 10}
-	w2 := worker{name: "w2", waitMs: 30}
-	l.AddWorker(w1)
-	l.AddWorker(w1)
-	l.AddWorker(w1)
-	l.AddWorker(w2)
-	l.AddWorker(w2)
-
-	var wg sync.WaitGroup
-	jobs := 12
-	wg.Add(jobs)
-	for i := 0; i < jobs; i++ {
-		l.Do(func(wk Worker) {
-			defer wg.Done()
-
-			switch w := wk.(type) {
-			case worker:
-				start := time.Now()
-				time.Sleep(time.Duration(w.waitMs) * time.Millisecond)
-				log.Printf("%s (%3dms)", w.name, time.Since(start).Nanoseconds()/1000000)
-			}
-
-		})
+func (w *worker) Do(ctx context.Context, j Job) error {
+	job, ok := j.(myJob)
+	if !ok {
+		return fmt.Errorf("Do error")
 	}
 
-	wg.Wait()
+	log.Printf("start %d", job.index)
+
+	time.Sleep(time.Duration(job.index*50) * time.Millisecond)
+
+	select {
+	case <-ctx.Done():
+		log.Printf("job %2d worker %s %s", job.index, w.name, ctx.Err().Error())
+		return ctx.Err()
+	default:
+	}
+
+	log.Printf("done  %d", job.index)
+
+	return nil
+}
+
+func TestLimichan(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Millisecond)
+	defer cancel()
+
+	l := New(ctx, MaxWorker(5))
+
+	w1 := &worker{name: "w1", waitMs: 10}
+	w2 := &worker{name: "w2", waitMs: 30}
+	l.AddWorker(w1)
+	l.AddWorker(w1)
+	l.AddWorker(w1)
+	l.AddWorker(w2)
+	l.AddWorker(w2)
+
+	jobs := 12
+	for i := 0; i < jobs; i++ {
+		i := i
+		<-time.After(5 * time.Millisecond)
+
+		err := l.Do(myJob{index: i})
+
+		if err == context.DeadlineExceeded {
+			// skip
+			log.Printf("job %2d %s", i, err.Error())
+		} else if err != nil {
+			log.Printf("job %2d %s", i, err.Error())
+		}
+	}
+
+	l.Wait()
+
+	if len(l.Errors()) > 0 {
+		log.Print("--")
+		for _, err := range l.Errors() {
+			log.Print(err.Error())
+		}
+	}
 
 	//t.Error("error")
 }
